f28708e83247351b222c169df8dd513f0c791332
taek95 <01077275247a@gmail.com>
2024-02-16 09:00:29 +0900

    finally deploy 7

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
+REPOSITORY=/home/ubuntu/app/step2
+PROJECT_NAME=deploy
 
 echo "> Build 파일 복사"
 
-cp /home/ubuntu/app/step2/zip/*.jar /home/ubuntu/app/step2/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-echo "현재 구동중인 어플리케이션 pid: $(pgrep -fl deploy | grep jar | awk '{print $1}')"
+# 수행 중인 애플리케이션 프로세스 ID => 구동 중이면 종료하기 위함
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
-if [ -z "$(pgrep -fl deploy | grep jar | awk '{print $1}')" ]; then
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -15 $(pgrep -fl deploy | grep jar | awk '{print $1}')"
-    kill -15 $(pgrep -fl deploy | grep jar | awk '{print $1}')
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
     sleep 5
 fi
-#//d
+
 echo "> 새 어플리케이션 배포"
 
-echo "> JAR Name: $(ls -tr /home/ubuntu/app/step2/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> $(ls -tr /home/ubuntu/app/step2/*.jar | tail -n 1) 에 실행권한 추가"
+echo "> JAR Name: $JAR_NAME"
 
-chmod +x $(ls -tr /home/ubuntu/app/step2/*.jar | tail -n 1) # Jar 파일은 실행 권한이 없는 상태이므로 권한 부여
+echo "> $JAR_NAME 에 실행권한 추가"
 
-echo "> $(ls -tr /home/ubuntu/app/step2/*.jar | tail -n 1) 실행"
+chmod +x $JAR_NAME # Jar 파일은 실행 권한이 없는 상태이므로 권한 부여
+
+echo "> $JAR_NAME 실행"
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties \
     -Dspring.profiles.active=real \
-    $(ls -tr /home/ubuntu/app/step2/*.jar | tail -n 1) > /home/ubuntu/app/step2/nohup.out 2>&1 &
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
