0ae07f83c5c5130b82b74a162013a612ef6ecbef
taek95 <01077275247a@gmail.com>
2024-02-20 14:12:17 +0900

    aaaaaa5525aa1a

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-REPOSITORY=/home/ubuntu/app/step2
-PROJECT_NAME=deploy
 
-echo "> Build 파일 복사"
+REPOSITORY=/home/ubuntu/app/step2/zip
+#PROJECT_NAME=deploy
 
-cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
-
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-# 수행 중인 애플리케이션 프로세스 ID => 구동 중이면 종료하기 위함
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
-
-echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
-fi
+#echo "> Build 파일 복사"
+#
+#cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
+#
+#echo "> 현재 구동중인 애플리케이션 pid 확인"
+#
+## 수행 중인 애플리케이션 프로세스 ID => 구동 중이면 종료하기 위함
+#CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+#
+#echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+#
+#if [ -z "$CURRENT_PID" ]; then
+#    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+#else
+#    echo "> kill -15 $CURRENT_PID"
+#    kill -15 $CURRENT_PID
+#    sleep 5
+#fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/build/libs/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
