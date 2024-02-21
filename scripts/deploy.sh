ded6314aa1c8e5982b468014bce01837ef7a4be6
taek95 <01077275247a@gmail.com>
2024-02-21 09:54:24 +0900

    auto deploy 1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
+REPOSITORY=/home/ubuntu/app/
 
-REPOSITORY=/home/ubuntu/app/step2
-#PROJECT_NAME=deploy
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-#echo "> Build 파일 복사"
-#
-#cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
-#
-#echo "> 현재 구동중인 애플리케이션 pid 확인"
-#
-## 수행 중인 애플리케이션 프로세스 ID => 구동 중이면 종료하기 위함
-#CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
-#
-#echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
-#
-#if [ -z "$CURRENT_PID" ]; then
-#    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-#else
-#    echo "> kill -15 $CURRENT_PID"
-#    kill -15 $CURRENT_PID
-#    sleep 5
-#fi
+# 수행 중인 애플리케이션 프로세스 ID => 구동 중이면 종료하기 위함
+CURRENT_PID=$(pgrep -fl java  | grep jar | awk '{print $1}')
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/zip/build/libs/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
