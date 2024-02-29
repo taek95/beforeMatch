82506622d9d3337304fca447eb23e112d363030d
taek95 <01077275247a@gmail.com>
2024-02-29 11:51:25 +0900

    sidbkal

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,6 +28,8 @@ chmod +x $JAR_NAME # Jar 파일은 실행 권한이 없는 상태이므로 권�
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $JAR_NAME > /dev/null 2>&1 &
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.yml,/home/ubuntu/app/application.yml \
+    $JAR_NAME > /dev/null 2>&1 &
+#nohup java -jar $JAR_NAME > /dev/null 2>&1 &
 
-#\ -Dspring.config.location=classpath:./application.yml \
