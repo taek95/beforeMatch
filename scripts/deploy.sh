6614d488c9c867c7edde80326e11b09bfa73a141
taek95 <01077275247a@gmail.com>
2024-02-29 11:35:26 +0900

    lpea

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,4 +28,4 @@ chmod +x $JAR_NAME # Jar 파일은 실행 권한이 없는 상태이므로 권�
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $JAR_NAME > /dev/null 2>&1 &
+nohup java -jar \ -Dspring.config.location=classpath:./application.yml \ $JAR_NAME > /dev/null 2>&1 &
