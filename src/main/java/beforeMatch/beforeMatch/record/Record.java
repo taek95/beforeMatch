b19abdd818d264b83f24e7cdda4f5605f0013c2c
taek95 <01077275247a@gmail.com>
2024-03-16 16:57:16 +0900

    make record10

--- src/main/java/beforeMatch/beforeMatch/record/Record.java
+++ src/main/java/beforeMatch/beforeMatch/record/Record.java
@@ -2,8 +2,11 @@ package beforeMatch.beforeMatch.record;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter @Setter
 public class Record {
 
     @Id
