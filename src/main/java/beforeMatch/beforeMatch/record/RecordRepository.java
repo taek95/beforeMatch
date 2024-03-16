79d27abe218a29b7d07b6f5ea2d2bec7624cf0ef
taek95 <01077275247a@gmail.com>
2024-03-16 16:54:44 +0900

    make record9

--- src/main/java/beforeMatch/beforeMatch/record/RecordRepository.java
+++ src/main/java/beforeMatch/beforeMatch/record/RecordRepository.java
@@ -1,4 +1,17 @@
 package beforeMatch.beforeMatch.record;
 
-public class RecordRepository {
+import beforeMatch.beforeMatch.board.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record, String> {
+
+//    // 페이징 기능
+//    @Override
+//    Page<Board> findAll(Pageable pageable);
+
 }
+
--- src/main/java/beforeMatch/beforeMatch/record/RecordService.java
+++ src/main/java/beforeMatch/beforeMatch/record/RecordService.java
@@ -1,4 +1,23 @@
 package beforeMatch.beforeMatch.record;
 
+
+import beforeMatch.beforeMatch.board.Board;
+import beforeMatch.beforeMatch.board.BoardRepository;
+import beforeMatch.beforeMatch.member.Member;
+import beforeMatch.beforeMatch.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class RecordService {
+    private final RecordRepository recordRepository;
+
+    public void save(Record record) {
+        recordRepository.save(record);
+    }
 }
