788abf3882aea1edb565244d09b82a3cf375eb55
taek95 <01077275247a@gmail.com>
2024-03-16 15:45:56 +0900

    make record3

--- src/main/java/beforeMatch/beforeMatch/record/recordController.java
+++ src/main/java/beforeMatch/beforeMatch/record/recordController.java
@@ -1,13 +1,20 @@
 package beforeMatch.beforeMatch.record;
 
+import beforeMatch.beforeMatch.login.Login;
+import beforeMatch.beforeMatch.login.SessionConst;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class recordController {
 
     @GetMapping("/record/list")
-    public String record() {
+    public String record(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Login loginMember,
+            Model model) {
+        model.addAttribute("loginMember", loginMember);
         return "record/recordList";
     }
 }
