788bf93d0b005a87778def0a326e977626c3640f
taek95 <01077275247a@gmail.com>
2024-03-21 10:39:09 +0900

    add datetime2

--- src/main/java/beforeMatch/beforeMatch/BeforeMatchApplication.java
+++ src/main/java/beforeMatch/beforeMatch/BeforeMatchApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+@EnableJpaAuditing // auditing 기능 사용하기 위해
 @SpringBootApplication
 public class BeforeMatchApplication {
 
