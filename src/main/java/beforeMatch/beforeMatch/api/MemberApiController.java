ab2a9a73f8b5e235a0291965284b27f434c8044e
taek95 <01077275247a@gmail.com>
2024-04-24 22:37:57 +0900

    api4

--- src/main/java/beforeMatch/beforeMatch/api/MemberApiController.java
+++ src/main/java/beforeMatch/beforeMatch/api/MemberApiController.java
@@ -2,36 +2,60 @@ package beforeMatch.beforeMatch.api;
 
 import beforeMatch.beforeMatch.member.Member;
 import beforeMatch.beforeMatch.member.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-// 회원 가입 api
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
 
+    // 회원 등록 api
     @PostMapping("/api/members")
     public CreateMemberResponse saveMember(@RequestBody CreateMemberRequest request) {
         Member member = new Member();
-        member.setMemberName(request.getName());
-        int id = memberService.join(member);
-        return new CreateMemberResponse(id);
+        member.setMemberName(request.getMemberName());
+        member.setMemberId(request.getMemberId());
+        String memberId = memberService.join(member);
+        return new CreateMemberResponse(memberId);
     }
+
+    // 회원 수정 api, memberId가 들어오면 memberName을 수정
+    @PutMapping("/api/members/{memberId}")
+    public UpdateMemberResponse updateMember(
+            @PathVariable("memberId") String memberId, @RequestBody UpdateMemberRequest request) {
+        memberService.update(memberId, request.getMemberName());
+        Member findMember = memberService.findOne(memberId);
+        return new UpdateMemberResponse(findMember.getMemberId(),findMember.getMemberName());
+    }
+
     @Data
     public static class CreateMemberRequest {
-        private String name;
+        private String memberName;
+        private String memberId;
     }
 
     @Data
     public static class CreateMemberResponse {
-        private int id;
-        public CreateMemberResponse(int id) {
-            this.id = id;
+        private String memberId;
+        public CreateMemberResponse(String memberId) {
+            this.memberId = memberId;
         }
+    }
+
+    @Data
+    public static class UpdateMemberRequest {
+        private String memberName;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class UpdateMemberResponse {
+        private String memberId;
+        private String memberName;
     }
 }
--- src/main/java/beforeMatch/beforeMatch/member/MemberService.java
+++ src/main/java/beforeMatch/beforeMatch/member/MemberService.java
@@ -13,15 +13,22 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public int join(Member member) {
+    public String join(Member member) {
 
         validateDuplicateUser(member);
         memberRepository.save(member);
-        return member.getId();
+        return member.getMemberId();
     }
-
     private void validateDuplicateUser(Member member) {
         List<Member> findMembers = memberRepository.findByMemberId(member.getMemberId());
         if(!findMembers.isEmpty()) throw new IllegalStateException("이미 존재하는 아이디입니다.");
+    }
+    public Member findOne(String memberId) {
+        return memberRepository.findOne(memberId);
+    }
+    @Transactional // 변경감지
+    public void update(String memberId, String name) {
+        Member member = memberRepository.findOne(memberId);
+        member.setMemberName(name);
     }
 }
